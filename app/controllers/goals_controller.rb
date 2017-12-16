b19ad6191c9610a56d5e8c748212dec3047809d0
jlmack13 <jennimacklin@gmail.com>
2017-12-16 10:46:37 -0500

    added actions to goals controller and created views

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,2 +1,38 @@
 class GoalsController < ApplicationController
+  before_action :set_goal, only: [:show, :edit, :update]
+
+  def new
+    @goal = Goal.new
+  end
+
+  def create
+    @goal = Goal.create(goal_params)
+    redirect_to goal_path(@goal)
+  end
+
+  def show
+    @actions = @goal.actions
+  end
+
+  def edit
+  end
+
+  def update
+    @goal.update(goal_params)
+    redirect_to goal_path(@goal)
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_goal
+    @goal = Goal.find(params[:id])
+  end
+
+  def goal_params
+    params.require(:goal).permit(:name, :description, :category, :start_date, :end_date)
+  end
+
 end
