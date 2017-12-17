d10880f2bfc5835421e9e2d692131d044e57aa97
jlmack13 <jennimacklin@gmail.com>
2017-12-17 16:34:43 -0500

    added nested resource

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -2,7 +2,7 @@ class ActionsController < ApplicationController
 
   def index
     #TODO change this so that it's only the user's actions
-    @actions = Action.all
+    @actions = current_user.actions.all
   end
 
   def new
@@ -10,7 +10,13 @@ class ActionsController < ApplicationController
   end
 
   def create
-    @action = Action.create(action_params)
+    @goal = Goal.find_by(params[:goal_id])
+    @action = @goal.actions.build(action_params)
+    if @action.save
+      redirect_to goal_path(@goal)
+    else
+      render :new
+    end
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root 'home#index'
-  resources :actions
-  resources :goals
+  resources :goals do
+    resources :actions
+  end
+
+  resources :actions, only: [:index] 
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
