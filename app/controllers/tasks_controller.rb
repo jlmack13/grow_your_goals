979264537999e1a56ea77e7bd3c318e8528594f1
jlmack13 <jennimacklin@gmail.com>
2017-12-21 19:13:32 -0500

    cleaning up styling

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -52,6 +52,6 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params.require(:task).permit(:name, :status, :goal_id)
+    params.require(:task).permit(:name, :status, :description, :goal_id)
   end
 end
